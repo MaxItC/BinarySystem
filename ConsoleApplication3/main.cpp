12bbad8185257222ddd7301e7247c3add784ae4e
Max <kaplunov.maksym@gmail.com>
2024-04-05 09:46:08 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication3/main.cpp
@@ -0,0 +1,40 @@
+﻿#include "pch.h"
+#include<iostream>
+#include <cmath>
+#include <ctime>
+#include <conio.h>
+
+using namespace std;
+
+
+
+
+void main() {
+
+	setlocale(LC_ALL, "RU");
+
+	int x, y{0}, z{ 0 };
+	cout << "X: ";
+	cin >> x;
+	system("cls");
+
+	while (x > 0) {
+		z += (x % 2) * pow(10,y);
+		x /= 2;
+		y++;
+	}
+	
+	cout << "X: " << z <<  endl;
+}
+
+
+   
+ 
+
+
+
+
+
+
+
+
